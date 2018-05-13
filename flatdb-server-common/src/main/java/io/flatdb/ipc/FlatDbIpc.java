6b3d4649f046d177e4b57f34869d7c17079bbf05
Chemist <chemist777@gmail.com>
2018-05-13 22:17:11 +0300

    pipe works

--- flatdb-server-common/src/main/java/io/flatdb/ipc/FlatDbIpc.java
+++ flatdb-server-common/src/main/java/io/flatdb/ipc/FlatDbIpc.java
@@ -1,24 +1,29 @@
 package io.flatdb.ipc;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class FlatDbIpc {
-    private final FileChannel serverChannel, clientChannel;
     public final Path serverPipePath, clientPipePath;
+    private final FileChannel serverChannel, clientChannel;
     private final ByteBufferConsumer receiveMethod, sendMethod;
 
     public FlatDbIpc(String fileNamePrefix, boolean serverMode) {
         try {
-            this.serverPipePath = makePipe(fileNamePrefix, true);
+            Path tmpPath = Paths.get(System.getProperty("java.io.tmpdir"));
+
+            this.serverPipePath = makePipe(tmpPath.resolve(fileNamePrefix + "server.pipe"));
             this.serverChannel = FileChannel.open(serverPipePath, serverMode ? StandardOpenOption.READ : StandardOpenOption.WRITE);
 
-            this.clientPipePath = makePipe(fileNamePrefix, false);
+            this.clientPipePath = makePipe(tmpPath.resolve(fileNamePrefix + "client.pipe"));
             this.clientChannel = FileChannel.open(clientPipePath, serverMode ? StandardOpenOption.WRITE : StandardOpenOption.READ);
 
             receiveMethod = serverMode ? serverChannel::read : clientChannel::read;
@@ -28,17 +33,23 @@ public class FlatDbIpc {
         }
     }
 
-    private Path makePipe(String fileNamePrefix, boolean server) throws IOException, InterruptedException {
-        Path pipePath = Paths.get(System.getProperty("java.io.tmpdir")).resolve(fileNamePrefix + (server?"server":"client") + ".pipe");
+    private static Path makePipe(Path pipePath) throws IOException, InterruptedException {
         if (!Files.exists(pipePath)) {
             Process process = new ProcessBuilder("mkfifo", pipePath.toAbsolutePath().toString())
-                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
-                    .redirectError(ProcessBuilder.Redirect.INHERIT)
+                    .redirectErrorStream(true)
                     .start();
             int exitCode = process.waitFor();
             //we should check for existence again because file can be created concurrently
             if (exitCode != 0 && !Files.exists(pipePath)) {
-                throw new RuntimeException("Can't mkfifo, exit code " + exitCode);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                try (InputStream is = process.getInputStream()) {
+                    byte[] buf = new byte[512];
+                    int len;
+                    while ((len = is.read(buf)) > 0) {
+                        baos.write(buf, 0, len);
+                    }
+                }
+                throw new RuntimeException("Can't mkfifo: " + new String(baos.toByteArray(), StandardCharsets.UTF_8));
             }
         }
         return pipePath;
@@ -64,12 +75,12 @@ public class FlatDbIpc {
         try {
             serverChannel.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can't close pipe", e);
+            throw new RuntimeException("Can't close server pipe " + serverPipePath, e);
         }
         try {
             clientChannel.close();
         } catch (IOException e) {
-            throw new RuntimeException("Can't close pipe", e);
+            throw new RuntimeException("Can't close client pipe " + clientPipePath, e);
         }
     }
 
